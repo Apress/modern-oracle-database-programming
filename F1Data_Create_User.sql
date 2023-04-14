1857c8f530a7f504053c84f684815204703f647b
Lucas Jellema <lucasjellema@gmail.com>
2023-04-14 12:43:59 +0200

    Add Gitpod workspace definition

--- F1Data_Create_User.sql
+++ F1Data_Create_User.sql
@@ -11,6 +11,9 @@
  * Filename : F1Data_Create_User.sql
  * Remarks  : Run this script as the admin user
  */
+
+ALTER SESSION SET CONTAINER=FREEPDB1 SERVICE=FREEPDB1;
+
 drop user f1data cascade
 /
 
